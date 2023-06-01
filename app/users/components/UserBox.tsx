572967a9b1fdb398884b40331d94d526cc93d248
ejiro1994 <ejiro1994@gmail.com>
2023-06-01 23:40:16 +0100

    loading states

--- app/users/components/UserBox.tsx
+++ app/users/components/UserBox.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import Avatar from '@/app/components/Avatar'
+import LoadingModal from '@/app/components/LoadingModal'
 import { User } from '@prisma/client'
 import axios from 'axios'
 import { useRouter } from 'next/navigation'
@@ -30,11 +31,15 @@ const UserBox: React.FC<UserBoxProps> = ({ data }) => {
   }, [data, router])
 
   console.log('data', data)
-  
 
-  return <div 
-    onClick={handleClick}
-    className='
+  return (
+    <>
+    {isLoading && (
+      <LoadingModal />
+    )}
+      <div
+        onClick={handleClick}
+        className='
     w-full
     relative
     flex
@@ -47,32 +52,34 @@ const UserBox: React.FC<UserBoxProps> = ({ data }) => {
     transition
     cursor-pointer
   '
-  >
-    <Avatar user={data}/>
-    <div className='min-w-0 flex-1'>
-      <div className="focus:outline-none">
-        <div 
-          className='
+      >
+        <Avatar user={data} />
+        <div className='min-w-0 flex-1'>
+          <div className='focus:outline-none'>
+            <div
+              className='
            flex
            justify-betwee
            items-center
            mb-1
           
         '
-        >
-          <p 
-            className='
+            >
+              <p
+                className='
              text-sm
              font-medium
              text-grey-900
           '
-          >
-            {data.name}
-          </p>
+              >
+                {data.name}
+              </p>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
+    </>
+  )
 }
 
 export default UserBox
